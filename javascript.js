3b271899ca3a9c69fd9ffa65f9ef94c84c0590b5
ageronimo <angelageroniimo@gmail.com>
2016-10-21 09:18:57 -1000

    aliens!

--- javascript.js
+++ javascript.js
@@ -77,3 +77,21 @@ function createGreeting (name, age){
 
 createGreeting('Tan', 18);
 // invoking function
+
+
+// ALIENS! project 
+var blue = 1000
+var green = 4000
+var red = 5000
+
+if (blue + green + red == 10000){
+  if (green > 5000){
+    console.log("Ugh why are they so ugly")
+  }
+  if (greed + red > 8000){
+    console.log("It's starting to feel a lot like Christmas")
+  }
+  if (red + blue <= 3000){
+    console.log("We are sooo dead dude.")
+  }
+}
